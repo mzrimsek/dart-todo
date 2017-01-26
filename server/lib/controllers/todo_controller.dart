bc591166e084a0f17c54eca270c861ced2c745af
Mike Zrimsek <mikezrimsek@gmail.com>
2017-01-26 13:45:34 -0500

    post working
    kind of questionable if this is how it should work. Just passing a string in the request body and decoding on the server feels weird, but maybe it's right. I'm surprised I don't need to pass it an entire object. I'll look into this more as I'm working on put.

--- server/lib/controllers/todo_controller.dart
+++ server/lib/controllers/todo_controller.dart
@@ -2,16 +2,28 @@ import '../dart_todo_server.dart';
 
 class TodoController extends HTTPController
 {
-  var todos = [
-    "Todo 1",
-    "Todo 2"
-  ];
-
   @httpGet
   getAllTodos() async
   {
     var todoQuery = new Query<Todo>();
     var todos = await todoQuery.fetch();
     return new Response.ok(todos);
+  }
+
+  @httpPost
+  createTodo() async 
+  {
+    request.decodeBody();
+    var todoText = request.requestBodyObject;
+    var insertQuery = new Query<Todo>()
+      ..values.text = todoText;
+    var newTodo = await insertQuery.insert();
+    return new Response.ok(newTodo);
+  }
+
+  @httpPut
+  updateTodo(int id, String text)
+  {
+
   }
 }
