b6d88c57b226e8c82ddd249977f3d164f6f58c0f
Mike Zrimsek <mikezrimsek@gmail.com>
2017-01-26 22:02:47 -0500

    attempting to merge projects so there's only one
    still want to keep client/server separation as much as possible

--- bin/start.dart
+++ bin/start.dart
@@ -1,12 +1,16 @@
-import 'package:dart_todo_server/dart_todo_server.dart';
+import '../server/lib/dart_todo_server.dart';
 
 void main()
 {
+  int port = 8080;
   var config = new TodoConfiguration("config.yaml");
+
   var app = new Application<TodoRequestSink>()
+    ..configuration.port = port
     ..configuration.configurationOptions = {
       TodoRequestSink.ConfigurationKey: config
     };
 
+  print("server started on port " + port.toString());
   app.start();
 }
--- test/todo_controller_test.dart
+++ test/todo_controller_test.dart
@@ -1,5 +1,5 @@
 import 'package:test/test.dart';
-import 'package:dart_todo_server/dart_todo_server.dart';
+import '../server/lib/dart_todo_server.dart';
 
 void main()
 {
