ada3e24173f9191142fa614eb39f3f9521a985af
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-14 14:02:42 -0500

    added hashlocation provider :fire:
    this solves the refresh and get a 404 problem

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,12 @@
-import 'package:http/http.dart';
-import 'package:http/browser_client.dart';
 import 'package:angular2/core.dart';
 import 'package:angular2/platform/browser.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 import '../lib/components/app_component/app_component.dart';
 
 void main()
 {
-  bootstrap(AppComponent,
+  bootstrap(AppComponent, 
     [provide(Client, useFactory: () => new BrowserClient(), deps: [])] 
   );
 }
